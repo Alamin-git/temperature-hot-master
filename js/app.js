06cfef6e44229636b455098c52f1bb1dc1c0a6d8
Alamin-git <alamin2.mas@gmail.com>
2022-03-04 23:15:48 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const API_KEY = `6c7e9ddfb146497d1e9e024b81fc6723`;
+
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data));
+
+};
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temp', temperature.main.temp);
+    setInnerText('stetus', temperature.weather[0].main);
+    // set icon 
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('image-icon');
+    imgIcon.setAttribute('src', url);
+    console.log(temperature);
+}
